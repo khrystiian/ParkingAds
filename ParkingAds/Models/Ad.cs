7872624e6fa88eaaaaea4ee2823bd1dad9d6b739
David Jahanshiri <1061797@ucn.dk>
2018-12-05 10:39:50 +0100

    Ads

--- ParkingAds/Models/Ad.cs
+++ ParkingAds/Models/Ad.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace Models
 {
+    [XmlRoot(ElementName = "Ad", Namespace = "http://schemas.datacontract.org/2004/07/PSUNCAdService.Models")]
     public class Ad
     {
-        public string ImageData;
+        [XmlElement(ElementName = "ImageData", Namespace = "http://schemas.datacontract.org/2004/07/PSUNCAdService.Models")]
+        public string ImageData { get; set; }
+        [XmlAttribute(AttributeName = "i", Namespace = "http://www.w3.org/2000/xmlns/")]
+        public string I { get; set; }
+        [XmlAttribute(AttributeName = "xmlns")]
+        public string Xmlns { get; set; }
+
+        public DateTime TimeStamp { get; set; }
     }
+
 }
--- ParkingAds/ParkingAds/AdLogic.cs
+++ ParkingAds/ParkingAds/AdLogic.cs
@@ -2,33 +2,64 @@
 using System;
 using System.Data;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace ParkingAds.BusinessLogic
 {
     public class AdLogic : IAdLogic
     {
+        public Ad cachedAd = null;
         public string GetAd()
         {
-            return CacheAd();
+            if (cachedAd==null || (cachedAd.TimeStamp < DateTime.UtcNow.AddMinutes(-2)))
+            {
+                if (cachedAd != null)
+                {
+                    System.Diagnostics.Debug.WriteLine((cachedAd.TimeStamp < DateTime.UtcNow.AddMinutes(-2)));
+                    System.Diagnostics.Debug.WriteLine(DateTime.UtcNow.AddMinutes(-2));
+                    System.Diagnostics.Debug.WriteLine((cachedAd.TimeStamp));
+                }
+
+
+
+                return CacheAd();
+            }
+            else
+            {
+                return cachedAd.ImageData;
+            }
         }
 
         public string CacheAd()
         {
             using (var httpClient = new HttpClient())
             {
-                var response = httpClient.GetStringAsync(new Uri("http://adservice.ws.dm.sof60.dk/api/ad")).Result;
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
+                try
+                {
+                    var data = httpClient.GetAsync("http://adservice.ws.dm.sof60.dk/api/ad").Result;
 
-                var doc = XDocument.Parse(response);
+                    var ser = new XmlSerializer(typeof(Ad));
+                    var t = (Ad)ser.Deserialize(data.Content.ReadAsStreamAsync().Result);
+
+                    t.TimeStamp = DateTime.Now;
+                    cachedAd = t;
+                    return t.ImageData;
+                }
+                catch
+                {
+                    if (cachedAd==null)
+                    {
+                        return "imagine this is a placeholder base64 string";
+                    }
+                    return cachedAd.ImageData;
+                }
 
 
-                var test = doc.Root.Elements("ImageData");
-                return "lol";
-
-                
             }
         }
-
 
     }
 }
